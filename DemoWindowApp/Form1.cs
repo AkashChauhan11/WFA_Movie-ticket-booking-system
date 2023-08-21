340603d4b82c39640fb9e644bd5515a43864d489
akashchauhan191311@gmail.com <cakash701@rku.ac.in>
2023-08-21 20:08:29 +0530

    Static Movie Screen completed

--- DemoWindowApp/Form1.cs
+++ DemoWindowApp/Form1.cs
@@ -15,62 +15,152 @@ namespace DemoWindowApp
         public Form1()
         {
             InitializeComponent();
+            firstRow(btn_clicked);
+            midRow(btn_clicked);
+            lastRow(btn_clicked);
+            setMovieAndTime();
+            
 
         }
 
+        double price=0;
+        double firstRowSeatPrice = 200;
+        double secondRowSeatPrice = 280;
+        double lastRowSeatPrice = 350;
+        List<string> selectedSeats= new List<string>();
+        int totalSeatSelected = 0;
+        string movieName = "Black Panther";
+        string time = "3 pm - 6 pm";
 
-        private void pictureBox1_Click(object sender, EventArgs e)
+        public void setMovieAndTime()
         {
-            if(pictureBox1.BackColor == Color.Green)
-            {
-                pictureBox1.BackColor = Color.Silver;
-
-            }
-            else
-            {
-            pictureBox1.BackColor = Color.Green;
-
-            }
+            lblMovie.Text= movieName;
+            lblTime.Text= time;
         }
 
-        private void pictureBox2_Click(object sender, EventArgs e)
-        {
-            if (pictureBox2.BackColor == Color.Green)
+
+
+        public void firstRow(EventHandler click) {
+            for(int i = 1; i <= 18; i++)
             {
-                pictureBox2.BackColor = Color.Silver;
+                Button btn = new System.Windows.Forms.Button();
+
+                btn.Size = new System.Drawing.Size(45, 35);
                
+                btn.BackColor= Color.Silver;
+                btn.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+                btn.Text = i.ToString();
+                this.frontRowLayout.Controls.Add(btn);
+
+                btn.Click += btn_clicked;
+                
+
+            }
+           
+        }
+
+
+        public void midRow(EventHandler click)
+        {
+            for (int i = 19; i <= 54; i++)
+            {
+                Button btn = new System.Windows.Forms.Button();
+
+                btn.Size = new System.Drawing.Size(45, 35);
+
+                btn.BackColor = Color.Silver;
+                btn.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+                btn.Text = i.ToString();
+                this.midRowLayout.Controls.Add(btn);
+
+                btn.Click += btn_clicked;
+
+
+            }
+        }
+
+        public void lastRow(EventHandler click)
+        {
+          
+            for (int i = 55; i <= 72; i++)
+            {
+                Button btn = new System.Windows.Forms.Button();
+
+                btn.Size = new System.Drawing.Size(45, 35);
+
+                btn.BackColor = Color.Silver;
+                btn.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+                btn.Text = i.ToString();
+                this.lastRowLayout.Controls.Add(btn);
+                btn.Click += btn_clicked;
+            }
+        }
+
+
+        public void btn_clicked(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            double ticketPrice = 0;
+            string seats = "";
+
+            if (  int.Parse( btn.Text) >=1 && int.Parse(btn.Text) <= 18)
+            {
+                ticketPrice = firstRowSeatPrice;
+            }else if(int.Parse(btn.Text) >= 19 && int.Parse(btn.Text) <= 54)
+            {
+                ticketPrice = secondRowSeatPrice;
             }
             else
             {
-                pictureBox2.BackColor = Color.Green;
-
+                ticketPrice = lastRowSeatPrice;
             }
 
+
+            if(btn.BackColor == Color.Green && btn.BackColor != Color.Red)
+            {
+                btn.BackColor = Color.Silver;
+                    price -= ticketPrice;
+                selectedSeats.Remove(btn.Text);
+                totalSeatSelected -= 1;
+            }
+            else
+            {
+                btn.BackColor = Color.Green;
+                    price += ticketPrice;
+                selectedSeats.Add(btn.Text);
+                totalSeatSelected += 1;
+            }
+
+            for(int i = 0; i <totalSeatSelected; i++)
+            {
+                
+                seats += selectedSeats[i]+" ,";
+                
+            }
+
+            lblPrice.Text="₹ "+price.ToString();
+            lblTotalSelSeat.Text=totalSeatSelected.ToString();
+            lblSelectedSeats.Text = seats;
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
         {
 
         }
 
-        private void label2_Click_1(object sender, EventArgs e)
+        private void pictureBox36_Click(object sender, EventArgs e)
         {
-
+            
         }
 
-        private void label2_Click_2(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label5_Click(object sender, EventArgs e)
+        private void pictureBox33_Click(object sender, EventArgs e)
         {
 
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            lblPrice.Text = price.ToString();
         }
     }
 }
